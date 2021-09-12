694fb5b2053987ba5d63fbc3a21d6acb8f718530
jellyredfox <jellyredfox@gmail.com>
2021-09-12 21:08:39 +0300

    add len words in reverse.html

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -9,4 +8,7 @@ def home(request):
 def reverse(request):
     user_text = request.GET['message']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'message': user_text, 'reversedtext': reversed_text})
+    len_words = (len(user_text.strip().split(" ")))
+    result_len_words = f'{len_words} word' if len_words == 1 else f'{len_words} words'
+    return render(request, 'reverse.html', {'message': user_text, 'reversedtext': reversed_text,
+                                            'lenwords': result_len_words})
